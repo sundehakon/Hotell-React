ab651b24d87483212d03784bc6ac8ab14f9e3ea5
sundehakon <142574795+sundehakon@users.noreply.github.com>
2024-03-19 21:42:38 +0100

    Adding user schema for login

--- server.js
+++ server.js
@@ -4,9 +4,19 @@ const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
 
-<<<<<<< HEAD
 const app = express();
 app.use(cors());
 app.use(express.json());
-=======
->>>>>>> 088ed45cb89a901bb900a494ce749ff234bd309c
+
+mongoose.connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+
+const userSchema = new mongoose.Schema({
+    email: String, 
+    username: String, 
+    password: String,
+});
+
+const User = mongoose.model('User', userSchema);
