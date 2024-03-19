84c0634c5b96f9eea0605938a4f74c3080726764
sundehakon <142574795+sundehakon@users.noreply.github.com>
2024-03-19 21:37:50 +0100

    Commit

---
+++ server.js
@@ -0,0 +1,9 @@
+require('dotenv').config();
+
+const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose');
+
+const app = express();
+app.use(cors());
+app.use(express.json());
