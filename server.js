d48ee54a46930c646a830cb501cc145e45a3a2c6
sundehakon <142574795+sundehakon@users.noreply.github.com>
2024-03-20 08:49:29 +0100

    Fixed issues with running server - added necessary packages using yarn

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.use(express.json());
 mongoose.connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true
-});
+}).catch(error => console.error(error));
 
 const userSchema = new mongoose.Schema({
     email: String, 
--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/register" element={<InputForm />} />
+        <Route path="/" element={<InputForm />} />
       </Routes>
     </Router>
   );
