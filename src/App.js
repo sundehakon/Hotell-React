e295c46f6d4f45afa1ef3aad647eb330db57d4f0
sundehakon <142574795+sundehakon@users.noreply.github.com>
2024-03-20 15:01:22 +0100

    Imported material ui and added theme

--- src/App.js
+++ src/App.js
@@ -3,16 +3,20 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import RoomForm from './RoomForm';
 import Home from './Home';
 import RegisterForm from './RegisterForm';
+import { ThemeProvider } from '@mui/material/styles';
+import theme from './theme';
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/register' element={<RegisterForm />} />
-        <Route path='/rooms' element={<RoomForm />} />
-      </Routes>
-    </Router>
+    <ThemeProvider theme={theme}>
+      <Router>
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/register' element={<RegisterForm />} />
+          <Route path='/rooms' element={<RoomForm />} />
+        </Routes>
+      </Router>
+    </ThemeProvider>
   );
 }
 
