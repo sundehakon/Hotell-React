94587b344e766381e214436db1c6893fbb1b0d5e
sundehakon <142574795+sundehakon@users.noreply.github.com>
2024-03-20 15:04:07 +0100

    Removed packagelockjson file

--- src/Home.js
+++ src/Home.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
+import { Typography } from '@mui/material';
 
 const Home = () => {
     return (
         <div>
             <h1>Home</h1>
-            <p>Welcome to the hotel management system!</p>
+            <Typography>Welcome to the hotel management system!</Typography>
             <a href='/register'>Register</a>
             <a href='/rooms'>Rooms</a>
         </div>
