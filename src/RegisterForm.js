24c1c0e60144f3f87e1fc1cbad9c1afefca86d44
sundehakon <142574795+sundehakon@users.noreply.github.com>
2024-03-20 16:19:51 +0100

    Removed old form - fixed issue with button submit

--- src/RegisterForm.js
+++ src/RegisterForm.js
@@ -66,33 +66,6 @@ const RegisterForm = () => {
         </CardActions>
       </Card>
     </form>
-
-        /* <a href='/'>Home</a>
-            <label>
-                Email:
-                <input
-                    type="email"
-                    value={email}
-                    onChange={(event) => setEmail(event.target.value)}
-                />
-            </label>
-            <label>
-                Username:
-                <input
-                    type="text"
-                    value={username}
-                    onChange={(event) => setUsername(event.target.value)}
-                />
-            </label>
-            <label>
-                Password:
-                <input
-                    type="password"
-                    value={password}
-                    onChange={(event) => setPassword(event.target.value)}
-                />
-            </label>
-            <button type="submit">Submit</button> */
     );
 };
 
