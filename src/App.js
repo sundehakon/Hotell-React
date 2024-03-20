af5bd8df3590f2d4df89adc670b2f33d55abbf4b
sundehakon <142574795+sundehakon@users.noreply.github.com>
2024-03-20 10:43:14 +0100

    Added paths to home and rooms in app

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import './App.css';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import InputForm from './InputForm';
+import RoomForm from './RoomForm';
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<InputForm />} />
+        <Route path='/' element={<Home />} />
+        <Route path='/register' element={<InputForm />} />
+        <Route path='/rooms' element={<RoomForm />} />
       </Routes>
     </Router>
   );
