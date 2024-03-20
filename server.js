3b191482fa66b08fec51af21bc72aa50593907a4
sundehakon <142574795+sundehakon@users.noreply.github.com>
2024-03-20 09:01:08 +0100

    Removed unnecessary code lines in server

--- server.js
+++ server.js
@@ -9,8 +9,6 @@ app.use(cors());
 app.use(express.json());
 
 mongoose.connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
 }).catch(error => console.error(error));
 
 const userSchema = new mongoose.Schema({
