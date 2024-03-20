c353165b6158008385a6a5a6958e4737b21902cb
sundehakon <142574795+sundehakon@users.noreply.github.com>
2024-03-20 10:47:40 +0100

    Inserted links on homepage to register and rooms

--- src/Home.js
+++ src/Home.js
@@ -1,10 +1,13 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 
 const Home = () => {
     return (
         <div>
             <h1>Home</h1>
             <p>Welcome to the hotel management system!</p>
+            <a href='/register'>Register</a>
+            <a href='/rooms'>Rooms</a>
         </div>
     );
 }
