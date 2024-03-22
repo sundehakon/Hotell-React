74c6d4ac65519bcb1b631794ca949c929b3b26d1
sundehakon <142574795+sundehakon@users.noreply.github.com>
2024-03-22 12:59:35 +0100

    Updating sidenav

--- src/SideNav.js
+++ src/SideNav.js
@@ -1,55 +1,57 @@
 import React, { useState } from "react";
-import { makeStyles } from '@mui/styles';
-import Box from '@mui/material/Box';
-import List from '@mui/material/List';
-import ListItem from '@mui/material/ListItem';
-import ListItemIcon from '@mui/material/ListItemIcon';
-import ListItemText from '@mui/material/ListItemText';
-import AppBar from '@mui/material/AppBar';
-import Toolbar from '@mui/material/Toolbar';
-import IconButton from '@mui/material/IconButton';
-import Typography from '@mui/material/Typography';
-import Drawer from '@mui/material/Drawer';
-import CssBaseline from '@mui/material/CssBaseline';
-import { Home, AssignmentInd, Apps, ContactMail, Menu } from '@mui/icons-material';
+import {
+  AppBar,
+  Toolbar,
+  Box,
+  IconButton,
+  List,
+  ListItem,
+  ListItemIcon,
+  ListItemText,
+  CssBaseline,
+  Drawer,
+  Typography,
+  Divider
+} from "@mui/material";
+import {
+  Apps,
+  Menu,
+  Home,
+  KingBed
+} from "@mui/icons-material";
+import { useNavigate } from "react-router-dom";
 
-const useStyles = makeStyles((theme) => ({
+const useStyles = () => ({
   menuSliderContainer: {
     width: 250,
     background: "#511",
     height: "100%"
   },
-  avatar: {
-    margin: "0.5rem auto",
-    padding: "1rem",
-    width: theme.spacing(13),
-    height: theme.spacing(13)
-  },
   listItem: {
     color: "tan"
   }
-}));
+});
 
 const listItems = [
   {
     listIcon: <Home />,
-    listText: "Home"
+    listText: "Home",
+    route: "/"
   },
   {
-    listIcon: <AssignmentInd />,
-    listText: "Rooms"
+    listIcon: <KingBed />,
+    listText: "Rooms",
+    route: "/rooms"
   },
   {
     listIcon: <Apps />,
-    listText: "Register"
-  },
-  {
-    listIcon: <ContactMail />,
-    listText: "Contacts"
+    listText: "Register",
+    route: "/register"
   }
 ];
 
 export default function SideNav() {
+  const navigate = useNavigate();
   const classes = useStyles();
   const [open, setOpen] = useState(false);
 
@@ -57,11 +59,24 @@ export default function SideNav() {
     setOpen(!open);
   };
 
+  const handleListPath = (route) => {
+    navigate(route);
+    setOpen(false);
+  }
+
   const sideList = () => (
-    <Box className={classes.menuSliderContainer} component="div">
+    <Box 
+      className={classes.menuSliderContainer} 
+      component="div" 
+      sx={{ width: 200 }}
+    >
+    <Box sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center', flexDirection: 'column', padding: 1 }}>
+      <Typography variant="h4">Hotell</Typography>
+    </Box>
+      <Divider />
       <List>
         {listItems.map((listItem, index) => (
-          <ListItem className={classes.listItem} button key={index}>
+          <ListItem className={classes.listItem} button key={index} onClick={() => handleListPath(listItem.route)}>
             <ListItemIcon className={classes.listItem}>
               {listItem.listIcon}
             </ListItemIcon>
@@ -75,19 +90,22 @@ export default function SideNav() {
   return (
     <>
       <CssBaseline />
+
       <Box component="nav">
         <AppBar position="static">
           <Toolbar>
             <IconButton onClick={toggleSlider}>
               <Menu />
             </IconButton>
-            <Typography variant="h6">Portfolio</Typography>
+            <Box sx={{ flexGrow: 1, display: 'flex', justifyContent: 'center' }}>
+              <Typography variant="h4" sx={{ textAlign: 'center', gap: 10 }}>Hotell</Typography>
+            </Box>
+            <Drawer open={open} anchor="left" onClose={toggleSlider}>
+              {sideList()}
+            </Drawer>
           </Toolbar>
         </AppBar>
       </Box>
-      <Drawer open={open} anchor="right" onClose={toggleSlider}>
-        {sideList()}
-      </Drawer>
     </>
   );
 }
