3edd4fa5e8d5d365174479d982da66b49cea02ae
sundehakon <142574795+sundehakon@users.noreply.github.com>
2024-03-24 18:50:46 +0100

    Starting development on profile form

--- src/Profile.js
+++ src/Profile.js
@@ -2,8 +2,8 @@ import { Typography } from "@mui/material";
 
 const Profile = () => {
     return (
-        <div>
-        <Typography variant="h3">Profile</Typography>
+        <div style={{ marginTop: -80 }}>
+        <Typography variant='h3' sx={{ textAlign: 'center', paddingBottom: 10 }}>Profile</Typography>
         </div>
     );
 };
