efe2f66b7f20a1ac9c7f8cf196944bf6f2e64f2f
sundehakon <142574795+sundehakon@users.noreply.github.com>
2024-03-26 17:01:47 +0100

    Testing commit

--- src/Profile.js
+++ src/Profile.js
@@ -1,9 +1,12 @@
-import { Typography } from "@mui/material";
+import { Grid, Typography } from "@mui/material";
 
 const Profile = () => {
     return (
         <div style={{ marginTop: -80 }}>
         <Typography variant='h3' sx={{ textAlign: 'center', paddingBottom: 10 }}>Profile</Typography>
+        <Grid>
+            
+        </Grid>
         </div>
     );
 };
