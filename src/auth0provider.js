f1139d0faabeebfc598c97c1165b7250f99b6528
sundehakon <142574795+sundehakon@users.noreply.github.com>
2024-04-04 17:58:05 +0200

    Adding back auth0provider file

---
+++ src/auth0provider.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { Auth0Provider } from '@auth0/auth0-react';
+
+const Auth0ProviderWithHistory = ({ children }) => {
+  const domain = process.env.REACT_APP_AUTH0_DOMAIN;
+  const clientId = process.env.REACT_APP_AUTH0_CLIENT_ID;
+
+  const onRedirectCallback = (appState) => {
+    const redirectPath = appState?.returnTo || window.location.pathname;
+    window.location.href = redirectPath;
+  };
+
+  return (
+    <Auth0Provider
+      domain={domain}
+      clientId={clientId}
+      redirectUri={window.location.origin}
+      onRedirectCallback={onRedirectCallback}
+    >
+      {children}
+    </Auth0Provider>
+  );
+};
+
+export default Auth0ProviderWithHistory;
