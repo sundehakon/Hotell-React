305eefe2b52022f60f409059fbf56f9e7dbce171
sundehakon <142574795+sundehakon@users.noreply.github.com>
2024-04-04 12:45:41 +0200

    Starting development on profile page with auth0 user

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,36 +1,11 @@
-import { Grid, Typography, Button, Card } from "@mui/material";
-import Cookies from "js-cookie";
+import React from 'react';
+import { useAuth0 } from '@auth0/auth0-react';
 
 const Profile = () => {
+    const { user, isLoading, isAuthenticated } = useAuth0();
 
-    // Fetches values stored as cookies in browser
-    const username = Cookies.get('username');
-    const firstName = Cookies.get('firstName');
-
-    // Function for removing stored cookies from browser
-    const removeCookies = () => {
-        Cookies.remove('username');
-        Cookies.remove('firstName');
-        window.location.reload();
-    }
-
-    if (username) {
-        return (
-            <div style={{ marginTop: -80 }}>
-            <Typography variant='h3' sx={{ textAlign: 'center', paddingBottom: 10 }}>Profile</Typography>
-            <Grid container spacing={1} gap={4}>
-                <Card>
-                    <Button onClick={removeCookies} variant='contained' sx={{ display: 'block', margin: 'auto' }}>Delete user</Button>
-                </Card>
-            </Grid>
-            </div>
-        );
-    } else {
-        return (
-            <div style={{ marginTop: -80 }}>
-            <Typography variant='h6' sx={{ textAlign: 'center', paddingBottom: 10 }}>Please register to view your profile</Typography>
-            </div>
-        )
+    if (isLoading) {
+        return <div>Loading...</div>
     }
 };
 
