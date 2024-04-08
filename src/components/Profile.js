21e7faf40b3189d0409d2a94f6d0bf8817b81add
sundehakon <142574795+sundehakon@users.noreply.github.com>
2024-04-08 08:54:33 +0200

    Fixing Profile code

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -7,6 +7,26 @@ const Profile = () => {
     if (isLoading) {
         return <div>Loading...</div>
     }
+
+    if (isAuthenticated) {
+        return (
+            isAuthenticated && (
+                <div>
+                    <img src={user.picture} alt={user.name} />
+                    <h2>{user.name}</h2>
+                    <p>{user.email}</p>
+                </div>
+            )
+        );
+    } else {
+        return (
+            <Box sx={{ textAlign: 'center', marginTop: 30 }}>
+                <Typography variant='h5'>
+                        You aren't logged in yet...
+                </Typography>
+            </Box>
+        );
+    }
 };
 
 export default Profile;
