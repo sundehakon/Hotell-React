8495d532c74144ee615402ff9479c50e55667c78
sundehakon <142574795+sundehakon@users.noreply.github.com>
2024-04-10 08:14:13 +0200

    Getting order information from user in server

--- server.js
+++ server.js
@@ -4,13 +4,15 @@ const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const expressJwt = require('express-jwt');
+const jwtAuthz = require('express-jwt-authz');
+const jwksRsa = require('jwks-rsa');
 
 const app = express();
 app.use(cors());
 app.use(bodyParser.json());
 
-mongoose.connect(process.env.MONGO_URI, {
-});
+mongoose.connect(process.env.MONGO_URI, {});
 const db = mongoose.connection;
 
 const reservationSchema = new mongoose.Schema({
@@ -21,18 +23,19 @@ const reservationSchema = new mongoose.Schema({
 
 const Reservation = mongoose.model('Reservation', reservationSchema);
 
-const authenticateJWT = expressJwt({
+expressJwt({
     secret: jwksRsa.expressJwtSecret({
       cache: true,
       rateLimit: true,
       jwksRequestsPerMinute: 5,
-      jwksUri: `https://${process.env.AUTH0_DOMAIN}/.well-known/jwks.json`
+      jwksUri: `https://${process.env.REACT_APP_AUTH0_DOMAIN}/.well-known/jwks.json`
     }),
     algorithms: ['RS256']
   });
 
 app.post('/api/Orders', async (req, res) => {
     const { checkInDate, checkOutDate } = req.body;
+    const userId = req.user.sub;
 
     const reservation = new Reservation({
         userId,
@@ -48,8 +51,25 @@ app.post('/api/Orders', async (req, res) => {
     }
 });
 
-app.get('/', (req, res) => {
-    res.send('Server is running...');
+app.get('/api/Orders', 
+    expressJwt({
+        secret: jwksRsa.expressJwtSecret({
+        cache: true,
+        rateLimit: true,
+        jwksRequestsPerMinute: 5,
+        jwksUri: `https://${process.env.REACT_APP_AUTH0_DOMAIN}/.well-known/jwks.json`
+    }),
+        algorithms: ['RS256']
+  }),
+  async (req, res) => {
+    const userId = req.user.sub;
+
+    try {
+        const reservations = await Reservation.find({ userId });
+        res.status(200).json(reservations);
+    } catch (err) {
+        res.status(500).send(err);
+    }
 });
 
 db.on('error', (error) => {
