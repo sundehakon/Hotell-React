de3c1c868a0b46e17b0c39d0b09d8729412c25c3
sundehakon <142574795+sundehakon@users.noreply.github.com>
2024-04-15 19:33:17 +0200

    Adding logic for checking existing order connected to user and displaying

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,9 +1,25 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useAuth0 } from '@auth0/auth0-react';
 import { Typography, Box, Card, Grid } from '@mui/material';
+import axios from 'axios';
 
 const Profile = () => {
     const { user, isLoading, isAuthenticated } = useAuth0();
+    const [orders, setOrder] = useState([]);
+
+    useEffect(() => {
+        const fetchOrders = async () => {
+            try {
+                const response = await axios.get('http://localhost:8080/api/Orders');
+                const matchingOrder = response.data.filter(order => order.userId === user.sub);
+                setOrder(matchingOrder);
+            } catch (error) {
+                console.error('Error fetching orders', error);
+            }
+        };
+
+        fetchOrders();
+    }, []);
 
     if (isLoading) {
         return <div>Loading...</div>;
@@ -24,7 +40,23 @@ const Profile = () => {
                     {user.picture && <img src={user.picture} alt={user.name} />}
                     <Typography variant='h3'>Welcome, {user.nickname}</Typography>
                     <Typography>E-mail: {user.email}</Typography>
+                    <Typography>User ID: {user.sub}</Typography>
                 </Card>
+                {orders && (
+                <Card>
+                    <Typography variant='h4'>Orders</Typography>
+                    <ul>
+                        {orders.map((order, index) => (
+                            <li key={index}>
+                                <p>User ID: {order.userId}</p>
+                                <p>Check-in Date: {order.checkInDate}</p>
+                                <p>Check-out Date: {order.checkOutDate}</p>
+                                <p>Room Type: {order.roomType}</p>
+                            </li>
+                        ))}
+                    </ul>
+                </Card>
+                )};
             </Grid>
         );
     } else {
