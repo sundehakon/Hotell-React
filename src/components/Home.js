ab697969232c6cec9caf7564bee59e1717d3805d
sundehakon <142574795+sundehakon@users.noreply.github.com>
2024-04-16 18:47:38 +0200

    Adding login buttons to homepage

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,14 +2,25 @@ import React from 'react';
 import Box from '@mui/material/Box';
 import Link from '@mui/material/Link';
 import Typography from '@mui/material/Typography';
+import LoginButton from './login';
+import LogoutButton from './logout';
+import { useAuth0 } from "@auth0/auth0-react";
 import '@fontsource/roboto/300.css';
 
 const Home = () => {
+    const {user, isLoading} = useAuth0();
+
     return (
         <Box sx={{ textAlign: 'center' }}>
             <Typography variant='h5' gutterBottom marginTop={3}>
                 Havblikk
             </Typography>
+            {!isLoading && !user && (
+              <LoginButton />
+            )}
+            {!isLoading && user && (
+              <LogoutButton />
+            )}
             <hr style={{ width: 1400 }}/>
             <Box sx={{ display: 'flex', justifyContent: 'center' }}>    
                 <Link href='/profile' sx={{ marginRight: 10 }}>Profile</Link>
