d28ef8071d6c5a5423672564d3c80845afc707d5
sundehakon <142574795+sundehakon@users.noreply.github.com>
2024-04-18 09:14:29 +0200

    Remove non used auth0 import

--- src/components/header.js
+++ src/components/header.js
@@ -9,7 +9,7 @@ import { useAuth0 } from "@auth0/auth0-react";
 import '@fontsource/roboto/300.css';
 
 const Header = () => {
-    const {user, isLoading, isAuthenticated } = useAuth0();
+    const {user, isLoading } = useAuth0();
 
     return (
         <Box sx={{ textAlign: 'center' }}>
