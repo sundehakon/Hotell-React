5eb53624b2523507f7bc47f297b35035b7a0d7ae
sundehakon <142574795+sundehakon@users.noreply.github.com>
2024-04-20 13:57:32 +0200

    Fix position and add background to 3D model on homepage

--- src/App.js
+++ src/App.js
@@ -21,8 +21,10 @@ function App() {
           element={
             <div>
               <Home />
-              <Box sx={{ width: 1000, height: 900, display: 'flex', alignItems: 'center', justifyContent: 'center', marginLeft: 40, zoom: '1' }}>
-                <Spline scene='https://prod.spline.design/aJK3vLcIjRFcByVZ/scene.splinecode' />
+              <Box sx={{ backgroundColor: '#1C232B', position: 'absolute', left: 0, right: 0, marginTop: 15, height: 800 }}>
+                <Box sx={{ width: 1000, height: 900, display: 'flex', alignItems: 'center', justifyContent: 'center', marginLeft: 45, zoom: '1' }}>
+                  <Spline scene='https://prod.spline.design/aJK3vLcIjRFcByVZ/scene.splinecode' />
+                </Box>
               </Box>
             </div>
             } />
