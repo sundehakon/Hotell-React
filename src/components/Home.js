748ee7bd4672348873cf73545daa3488ae2e3769
sundehakon <142574795+sundehakon@users.noreply.github.com>
2024-04-20 12:42:47 +0200

    Add tripadvisor photos and changed styling of mainpage

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,11 +5,11 @@ import '@fontsource/roboto/300.css';
 const Home = () => {
   return (
     <Container maxWidth="md">
-        <Typography sx={{ textAlign: 'center', marginTop: 10}}>
+        <Typography sx={{ textAlign: 'center', marginTop: 10, fontSize: 20}}>
             <Box sx={{ fontStyle: 'italic', display: 'inline', marginRight: 1 }}>"Experience luxury..."</Box> - Håkon Sunde (CEO)
         </Typography>
-      <Grid container spacing={3} sx={{ marginTop: 10 }}>
-        <Grid item xs={12} sm={6} md={4}>
+      <Grid container spacing={2} sx={{ marginTop: 10, marginLeft: 10 }}>
+        <Grid item xs={12} sm={6} md={9}>
           <Card>
             <CardMedia
               component="img"
@@ -29,7 +29,7 @@ const Home = () => {
             />
           </Card>
         </Grid>
-        <Grid item xs={12} sm={6} md={4}>
+        <Grid item xs={12} sm={6} md={5}>
           <Card>
             <CardMedia
               component="img"
@@ -40,6 +40,15 @@ const Home = () => {
           </Card>
         </Grid>
       </Grid>
+    <Box sx={{ backgroundColor: '#1C232B', position: 'absolute', left: 0, right: 0, marginTop: 20, height: 370 }}>
+        <Typography variant="h3" sx={{ textAlign: 'center', marginTop: 10, color: 'white' }}>Welcome to Havblikk</Typography>
+        <Typography variant="h5" sx={{ textAlign: 'center', marginTop: 5, color: 'white' }}>The place where you can relax and enjoy the beautiful view of the ocean</Typography>
+        <Typography variant="h5" sx={{ textAlign: 'center', marginTop: 5, color: 'white' }}>We offer a variety of rooms to suit your needs, whether you're traveling alone or with family</Typography>
+    </Box>
+    <Box>
+        <img src='/hotel-photos/trip-advisor-2022.webp' alt='Trip Advisor' style={{ width: 325, height: 250, display: 'block', marginLeft: 'auto', marginTop: 600 }} />
+        <img src='/hotel-photos/2020-travellers-choice.png' alt='Trip Advisor' style={{ width: 325, height: 250, display: 'block', marginRight: 'auto', marginTop: -250 }} />
+    </Box>
     </Container>
   );
 }
