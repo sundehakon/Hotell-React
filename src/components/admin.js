945a55e14f1aaf895c534d2b612cdc01b0578179
sundehakon <142574795+sundehakon@users.noreply.github.com>
2024-05-02 13:19:50 +0200

    Fix issue in useEffect, add auth0 user id to admin orders

--- src/components/admin.js
+++ src/components/admin.js
@@ -3,20 +3,20 @@ import { useEffect, useState } from "react";
 import axios from 'axios';
 
 const AdminPage = () => {
-    const [orders, setOrder] = useState([]);
+    const [orders, setOrders] = useState([]);
 
     useEffect(() => {
         const fetchOrders = async () => {
             try {
                 const response = await axios.get('http://localhost:8080/api/Orders');
-                setOrder(response);
+                setOrders(response.data); 
             } catch (error) {
                 console.error('Error fetching orders', error);
             }
         };
 
         fetchOrders();
-    });
+    }, []); 
 
     return (
         <div>
@@ -34,17 +34,20 @@ const AdminPage = () => {
                 marginLeft={45}
             >
                 {orders.map((order, index) => (
-                            <List key={index}>
-                                <ListItem>
-                                    <Box sx={{ fontWeight: 'bolder' }}>Check-in Date:</Box>&nbsp;{order.checkInDate}
-                                </ListItem>
-                                <ListItem>
-                                    <Box sx={{ fontWeight: 'bolder' }}>Check-out Date:</Box>&nbsp;{order.checkOutDate}
-                                </ListItem>
-                                <ListItem>
-                                    <Box sx={{ fontWeight: 'bolder' }}>Room Type:</Box>&nbsp;{order.roomType}
-                                </ListItem>
-                            </List>
+                    <List key={index}>
+                        <ListItem>
+                            <Box sx={{ fontWeight: 'bolder' }}>Check-in Date:</Box>&nbsp;{order.checkInDate}
+                        </ListItem>
+                        <ListItem>
+                            <Box sx={{ fontWeight: 'bolder' }}>Check-out Date:</Box>&nbsp;{order.checkOutDate}
+                        </ListItem>
+                        <ListItem>
+                            <Box sx={{ fontWeight: 'bolder' }}>Room Type:</Box>&nbsp;{order.roomType}
+                        </ListItem>
+                        <ListItem>
+                            <Box sx={{ fontWeight: 'bolder' }}>Auth0 User ID:</Box>&nbsp;{order.userId} 
+                        </ListItem>
+                    </List>
                 ))}
             </Grid>
         </div>
