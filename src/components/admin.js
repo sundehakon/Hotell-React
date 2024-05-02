1d438fe9dff998481db127db06d5c534a0f61736
sundehakon <142574795+sundehakon@users.noreply.github.com>
2024-05-02 13:10:57 +0200

    Write function for fetching every order from API

--- src/components/admin.js
+++ src/components/admin.js
@@ -1,8 +1,28 @@
 import { Typography } from "@mui/material";
+import { useEffect } from "react";
+import axios from 'axios';
 
 const AdminPage = () => {
+
+    useEffect(() => {
+        const fetchOrders = async () => {
+            try {
+                const response = await axios.get('http://localhost:8080/api/Orders');
+                console.log(response.data);
+            } catch (error) {
+                console.error('Error fetching orders', error);
+            }
+        };
+
+        fetchOrders();
+    });
+
     return (
-        <Typography>Test</Typography>
+        <div>
+            <Typography variant='h4' sx={{ textAlign: 'center' }} gutterBottom>
+                Admin Page
+            </Typography>
+        </div>
     );
 }
 
